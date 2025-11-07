0a36100fe586886593993e90080b621cfd62ea5a
Jeanne Viallard <viallard.jeanne@gmail.com>
2025-11-07 16:46:13 +0100

    Add nutrition infos

---
+++ src/components/NutritionInfo/index.jsx
@@ -0,0 +1,21 @@
+import "./NutritionInfo.scss";
+
+export default function NutritionInfo({ icon, bgColor, amount, unit, text }) {
+  return (
+    <div className="nutrition-info">
+      <div
+        className="nutrition-info__icon-container"
+        style={{ backgroundColor: bgColor }}
+      >
+        <img className="nutrition-info__icon" src={icon} alt="" />
+      </div>
+      <div>
+        <p className="nutrition-info__amount">
+          {amount.toLocaleString("en-US")}
+          {unit}
+        </p>
+        <p className="nutrition-info__type">{text}</p>
+      </div>
+    </div>
+  );
+}
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,10 +1,15 @@
 import { useParams } from "react-router";
 
 import SideBar from "../../components/SideBar";
+import NutritionInfo from "../../components/NutritionInfo";
 
 import useFetch from "../../utils/hooks/useFetch";
 
 import "./Profile.scss";
+import energyIcon from "../../assets/energy.png";
+import chickenIcon from "../../assets/chicken.png";
+import appleIcon from "../../assets/apple.png";
+import cheeseburgerIcon from "../../assets/cheeseburger.png";
 
 export default function Profile() {
   const params = useParams();
@@ -28,6 +33,39 @@ export default function Profile() {
                 <p className="profile__congrats">
                   Félicitation ! Vous avez explosé vos objectifs hier 👏
                 </p>
+
+                <div className="profile__data">
+                  <div className="profile__nutrition-data">
+                    <NutritionInfo
+                      icon={energyIcon}
+                      bgColor="#FF01011A"
+                      amount={data.keyData.calorieCount}
+                      unit="kCal"
+                      text="Calories"
+                    />
+                    <NutritionInfo
+                      icon={chickenIcon}
+                      bgColor="#4AB8FF1A"
+                      amount={data.keyData.proteinCount}
+                      unit="g"
+                      text="Proteines"
+                    />
+                    <NutritionInfo
+                      icon={appleIcon}
+                      bgColor="#F9CE231A"
+                      amount={data.keyData.carbohydrateCount}
+                      unit="g"
+                      text="Glucides"
+                    />
+                    <NutritionInfo
+                      icon={cheeseburgerIcon}
+                      bgColor="#FD51811A"
+                      amount={data.keyData.lipidCount}
+                      unit="g"
+                      text="Lipides"
+                    />
+                  </div>
+                </div>
               </>
             )}
           </div>
